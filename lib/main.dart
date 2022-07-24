d34faecf73057e1f52abbc1a3ccba9ede899ddd5
warahi <warahi@florence.digital>
2022-07-24 22:47:20 +1000

    feat(anonymous sign in): add functionality to authenticate an anonymous user

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:yabber/screens/wrapper.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() => runApp(const MyApp());
+void main() async {
+   WidgetsFlutterBinding.ensureInitialized();
+   await Firebase.initializeApp();
+   runApp(const MyApp());
+}
+
+
+// void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: Wrapper(),
     );
   }
--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:yabber/screens/authenticate/authenticate.dart';
-import 'package:yabber/screens/home/home.dart';
 
 class Wrapper extends StatelessWidget {
   const Wrapper({Key? key}) : super(key: key);
@@ -9,6 +8,6 @@ class Wrapper extends StatelessWidget {
   Widget build(BuildContext context) {
     
     // return either Home or Authenticate widget
-    return Authenticate();
+    return const Authenticate();
   }
 }
